e6c5c609631df88a966584ca8f61b35b75008b9b
andryan.syah <andryansyhh@gmail.com>
2025-10-06 14:20:46 +0700

    feat(auth): handler

---
+++ internal/handler/user_handler.go
@@ -0,0 +1,70 @@
+package handler
+
+import (
+	"auth-service/internal/domain/dto"
+	"auth-service/internal/middleware"
+	"auth-service/internal/usecase"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	userUc usecase.UserUsecase
+	jwt    *middleware.JWTManager
+}
+
+func NewUserHandler(userUc usecase.UserUsecase, jwt *middleware.JWTManager) *UserHandler {
+	return &UserHandler{userUc: userUc, jwt: jwt}
+}
+
+func (h *UserHandler) RegisterRoutes(r *gin.Engine) {
+	r.POST("/register", h.Register)
+	r.POST("/login", h.Login)
+
+	auth := r.Group("/account", middleware.AuthMiddleware(h.jwt))
+	auth.GET("/profile", h.Profile)
+}
+
+func (h *UserHandler) Register(c *gin.Context) {
+	var req dto.RegisterRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
+	}
+
+	if err := h.userUc.Register(req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "user registered"})
+}
+
+func (h *UserHandler) Login(c *gin.Context) {
+	var req dto.LoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+		return
+	}
+
+	resp, err := h.userUc.Login(req)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
+
+func (h *UserHandler) Profile(c *gin.Context) {
+	username := c.GetString("username")
+
+	resp, err := h.userUc.GetProfile(username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, resp)
+}
