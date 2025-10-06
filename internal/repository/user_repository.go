5d4ed6ca390bf902a91049284b814a4bf4a67df2
andryan.syah <andryansyhh@gmail.com>
2025-10-06 14:21:02 +0700

    feat(auth): repo

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"auth-service/internal/domain/model"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type UserRepository interface {
+	CreateUser(user *model.User) error
+	GetUserByUsername(username string) (*model.User, error)
+}
+
+type userRepo struct {
+	db *sqlx.DB
+}
+
+func NewRepository(db *sqlx.DB) UserRepository {
+	return &userRepo{db: db}
+}
+
+func (r *userRepo) CreateUser(user *model.User) error {
+	_, err := r.db.NamedExec(`
+		INSERT INTO users (username, password, created_at, created_by)
+		VALUES (:username, :password, NOW(), :created_by)
+	`, user)
+	return err
+}
+
+func (r *userRepo) GetUserByUsername(username string) (*model.User, error) {
+	var user model.User
+	err := r.db.Get(&user, "SELECT * FROM users WHERE username=$1 AND deleted_at IS NULL", username)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
